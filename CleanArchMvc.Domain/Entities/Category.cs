fd4a02b63a234f69e2d5491e6dfdc04e103ac310
liimarb1 <98990063+liimarb1@users.noreply.github.com>
2022-09-16 09:35:20 -0300

    Teste1

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+
+    //sealed = essa classe não pode ser herdada
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public Category(string name)
+        {
+            Name = name;
+        }
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
