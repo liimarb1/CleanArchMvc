2b8ce385ac00d0f5e127a26005196522e90a1ec2
liimarb1 <98990063+liimarb1@users.noreply.github.com>
2022-10-01 14:36:32 -0300

    Including NullReference in Product_image?.Lenght

--- CleanArchMvc.Domain.Tests/ProductUnitTest1.cs
+++ CleanArchMvc.Domain.Tests/ProductUnitTest1.cs
@@ -51,6 +51,12 @@ namespace CleanArchMvc.Domain.Tests
             action.Should().NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
         }
         [Fact]
+        public void CreateProduct_WithNullImage_NoNullReferenceException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should().NotThrow<NullReferenceException>();
+        }
+        [Fact]
         public void CreateProduct_WithEmptyImageName_NoDomainException()
         {
             Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "");
