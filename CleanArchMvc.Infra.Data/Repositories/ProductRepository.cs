4a517aa5c3a4a75143576d92dc5c8b9b4ff234bd
liimarb1 <98990063+liimarb1@users.noreply.github.com>
2022-10-02 12:33:03 -0300

    Create ProductRepository.cs

---
+++ CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+
+    public class ProductRepository : IProductCategory
+    {
+        ApplicationDbContext _productContext;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _productContext = context;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _productContext.Products.Add(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+           
+        }
+
+        public async Task<Product> GetByIdAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> GetProductCategoryAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RemoveAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
